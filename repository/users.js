1c4652cbd7383e749490c845e929fd8b566b8acb
Biruk Kalewold <birukkalewoldasrat@gmail.com>
2023-08-29 23:32:45 +0300

    added password salting and hashing while creating a user

--- repository/users.js
+++ repository/users.js
@@ -1,6 +1,10 @@
-// Import the fs and crypto modules
+// Import the fs, crypto, and util modules
 const fs = require("fs");
 const crypto = require("crypto");
+const util = require("util");
+
+// Convert the crypto.scrypt function into a promise-based version using util.promisify
+const scrypt = util.promisify(crypto.scrypt);
 
 // Define a class that can manage access to a data store of users
 class UsersRepository {
@@ -34,10 +38,24 @@ class UsersRepository {
   // Define an async method that creates a new record and adds it to the file
   async create(user) {
     user.id = this.randomId();
+    const salt = crypto.randomBytes(8).toString("hex");
+    const buffer = await scrypt(user.password, salt, 64);
+
     const userRecords = await this.getAll();
-    userRecords.push(user);
+    const newUserRecord = {
+      ...user,
+      password: `${buffer.toString("hex")}.${salt}`,
+    };
+    userRecords.push(newUserRecord);
     await this.writeAll(userRecords);
-    return user;
+    return newUserRecord;
+  }
+
+  // Define an async method that compares two passwords by hashing and salting them
+  async comparePasswords(saved, supplied) {
+    const [hash, salt] = saved.split(".");
+    const hashOfSupplied = await scrypt(supplied, salt, 64);
+    return hashOfSupplied.toString("hex") === hash;
   }
 
   // Define an async method that writes an array of records to the file
@@ -100,5 +118,5 @@ class UsersRepository {
   }
 }
 
-// Export a new instance of the UsersRepository class with a default filename of users.json
+// Export a new instance of the UsersRepository class with a custom filename of usersStore.json
 module.exports = new UsersRepository("usersStore.json");
