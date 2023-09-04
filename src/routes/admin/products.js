07db9f5a23fb0e7cc72c22b39eb39fa933c8aeec
Biruk Kalewold <birukkalewoldasrat@gmail.com>
2023-09-04 11:51:48 +0300

    added file upload with multipart form

--- src/routes/admin/products.js
+++ src/routes/admin/products.js
@@ -1,19 +1,32 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
+const multer = require("multer");
 const producsRepo = require("../../../repository/products");
 const productForm = require("../../../views/admin/products/new");
 const { validateTitle, validatePrice } = require("./validators");
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/admin/products", (req, res) => {});
 router.get("/admin/products/new", (req, res) => {
   res.send(productForm({}));
 });
-router.post("/admin/products/new", validateTitle, validatePrice, (req, res) => {
-  const errors = validationResult(req);
-  console.log(errors);
-  res.send("product created");
-});
+router.post(
+  "/admin/products/new",
+  upload.single("image"),
+  validateTitle,
+  validatePrice,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.send(productForm({ errors }));
+    }
+    const image = req.file.buffer.toString("base64");
+    const { title, price } = req.body;
+    producsRepo.create({ title, price, image });
+    res.send("product created");
+  }
+);
 
 module.exports = router;
