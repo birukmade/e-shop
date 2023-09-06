c4e9b7606ed7ab41d680bcac0873e946d5fbd5b7
Biruk Kalewold <birukkalewoldasrat@gmail.com>
2023-09-06 17:03:35 +0300

    added checking authentication status to access admin features

--- src/routes/admin/middlewares.js
+++ src/routes/admin/middlewares.js
@@ -10,4 +10,12 @@ module.exports = {
       next();
     };
   },
+
+  requireAuth(req, res, next) {
+    if (!req.session.userId) {
+      return res.redirect("/signup");
+    }
+
+    next();
+  },
 };
--- src/routes/admin/products.js
+++ src/routes/admin/products.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 
-const { handleErrors } = require("./middlewares");
+const { handleErrors, requireAuth } = require("./middlewares");
 const producsRepo = require("../../../repository/products");
 const productForm = require("../../../views/admin/products/new");
 const productsList = require("../../../views/admin/products/index");
@@ -10,15 +10,16 @@ const { validateTitle, validatePrice } = require("./validators");
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get("/admin/products", async (req, res) => {
+router.get("/admin/products", requireAuth, async (req, res) => {
   const products = await producsRepo.getAll();
   res.send(productsList({ products }));
 });
-router.get("/admin/products/new", (req, res) => {
+router.get("/admin/products/new", requireAuth, (req, res) => {
   res.send(productForm({}));
 });
 router.post(
   "/admin/products/new",
+  requireAuth,
   upload.single("image"),
   validateTitle,
   validatePrice,
