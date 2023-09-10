6518773dbcc51a6fb51ef409c22e684bc28d7dfe
Biruk Kalewold <birukkalewoldasrat@gmail.com>
2023-09-10 15:45:59 +0300

    integrated cart related route to the main route flow

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
 const adminProductsRoute = require("./routes/admin/products");
 const productsRoute = require("./routes/products");
+const cartRoute = require("./routes/cart");
 
 const app = express();
 app.use(express.static("public"));
@@ -13,6 +14,7 @@ app.use(cookieSession({ keys: ["supersecretkey"] }));
 app.use(authRouter);
 app.use(adminProductsRoute);
 app.use(productsRoute);
+app.use(cartRoute);
 
 app.listen(3000, () => {
   console.log("listening on port 3000...");
